8ccacf2f6487cac91e84f630fd264a93506a95e9
David Losiewicz <dlosiewicz@athenahealth.com>
2020-01-02 10:35:23 -0500

    First commit

---
+++ src/build/precache-manifest.0c5c9b9de1596453ddeefb6b2baf7912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd588f830bdf559db31718f44e2adbd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2743c8342cddbd3f463b",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "a83ce300e0de56d9407d",
+    "url": "/static/js/2.ab57f58e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.ab57f58e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2743c8342cddbd3f463b",
+    "url": "/static/js/main.89543ef2.chunk.js"
+  },
+  {
+    "revision": "6e55ea343e46b8691e3d",
+    "url": "/static/js/runtime-main.b5470c2c.js"
+  }
+]);
